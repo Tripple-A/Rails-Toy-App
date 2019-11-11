dfec9682ecbab7fdaedf912fadb781093d556868
Biodun <biodun9@gmail.com>
2019-11-11 22:28:05 +0100

    linted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   has_many :microposts
   validates :name, presence: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
